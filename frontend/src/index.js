c888b815e00009dd75fbff32f3a40d22d4930184
swathi <swathibalasubramanyam@gmail.com>
2023-10-09 14:02:17 -0700

    Logic for fetching csrf token and setting it on sessionStorage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,22 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import csrfFetch, {restoreCSRF} from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production"){
   window.store = store;
+  window.csrfFetch = csrfFetch;
+}
+
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
 }
 
 function Root() {
@@ -22,9 +33,8 @@ function Root() {
   )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (sessionStorage.getItem("X-CSRF-Token") === null) {
+  restoreCSRF().then(renderApplication);
+} else {
+  renderApplication();
+};
